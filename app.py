1a10badeef195cb611663610e82c10a8551a42be
kkiritok <kvk4706@gmail.com>
2024-03-24 10:03:00 +0530

    added multilingual

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request,jsonify
 import requests
+from deep_translator import GoogleTranslator
 from dotenv import load_dotenv
+from langdetect import detect
 import os
 load_dotenv()
 #API_URL = "https://api-inference.huggingface.co/models/benkimz/agbrain"
@@ -9,7 +11,22 @@ load_dotenv()
 API_URL = os.getenv("API_URL")
 headers=os.getenv('headers')
 app = Flask(__name__)
+translator = GoogleTranslator(source='auto', target='en')
 
+def translate_text_with_special_chars(text, target_language):
+    # Replace special characters with unique placeholders
+    special_chars = {'\n': '||newline||'}  # You can add more special characters as needed
+    for char, placeholder in special_chars.items():
+        text = text.replace(char, placeholder)
+
+    # Translate text
+    translation = translator.translate(text, target_language=target_language)
+
+    # Restore special characters
+    for char, placeholder in special_chars.items():
+        translation = translation.replace(placeholder, char)
+
+    return translation
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -21,8 +38,12 @@ def home():
 @app.route('/query', methods=['POST'])
 def get_response():
     user_input = request.form['user_input']
-    output = query({"inputs": user_input})
-    bot_response = output
+    user_language = detect(user_input)
+    translator = GoogleTranslator(source='auto', target=user_language)
+    user_input_en = translate_text_with_special_chars(user_input, target_language='en')
+    output = query({"inputs": user_input_en})
+    bot_response_en = output[0]['generated_text']
+    bot_response = translator.translate(bot_response_en, target_language=user_language)
     return jsonify({'bot_response': bot_response})
 if __name__ == "__main__":
     app.run(debug=True)
