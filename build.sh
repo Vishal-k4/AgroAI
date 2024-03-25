43976297b86faa7235a3a1cd563ef79ade2260c4
kkiritok <kvk4706@gmail.com>
2024-03-25 13:37:25 +0530

    updated bash

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-pacman -S portaudio python-pyaudio
+pip install virtualenv
+
+# Create a virtual environment
+python -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
 
 pip install -r requirements.txt --break-system-packages
