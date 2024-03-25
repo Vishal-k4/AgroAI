50c10c0772fdeae93c259d251ad1283baf3b5187
kkiritok <kvk4706@gmail.com>
2024-03-25 16:45:29 +0530

    updated bash

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
  # Build the wheel
 pip install -r requirements.txt --break-system-packages
+pip install .
 pip install toml && python -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip download -r /dev/stdin  --dest=dest
 
 
