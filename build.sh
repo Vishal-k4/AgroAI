9e4284993480fc72f92c2f53be3b0f0bf2122a8b
kkiritok <kvk4706@gmail.com>
2024-03-25 14:44:30 +0530

    updated bash

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
  # Build the wheel
 
 pip install -r requirements.txt --break-system-packages
-pip install wheel  # Install wheel if you haven't already  # Download PyAudio source distribution
-tar -xvf PyAudio-0.2.14.tar.gz  # Extract the source files
-cd PyAudio-0.2.14  # Navigate to the extracted directory
-python setup.py bdist_wheel --plat-name=manylinux1_x86_64
+pip install pip-tools
+python -m piptools compile \
+    -o requirements.txt \
+    pyproject.toml
