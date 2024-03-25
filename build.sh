0a08d348659f04872098949f4ef52ef5b665d8e0
kkiritok <kvk4706@gmail.com>
2024-03-25 14:57:33 +0530

    updated bash

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
  # Build the wheel
 
 #pip install -r requirements.txt --break-system-packages
-python setup.py install
-
+pip install pipwin
+pipwin install pyaudio
 pip install PyAudio --break-system-packages
