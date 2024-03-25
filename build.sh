787f4f9a3506156e5613ec51b1a6db3817d48b67
kkiritok <kvk4706@gmail.com>
2024-03-25 16:30:56 +0530

    updated bash

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
  # Build the wheel
-pip install pyproject.toml
 pip install -r requirements.txt --break-system-packages
+cd pd
+python setup.py build
+pip install PyAudio --break-system-packages
+
 
 
 
