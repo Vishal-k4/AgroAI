b0ddf67ddae4cce59a9deab98182c6b4de5f6e78
kandregula vishal karthik <110188306+kkiritok@users.noreply.github.com>
2024-03-26 12:18:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 pip instsll wheel
-pip install pipwin
-pipwin install pyaudio
+pip install pyaudio==0.2.11
+pip install portaudio==19.6.0
+
 pip install -r requirements.txt --break-system-packages
 
 
